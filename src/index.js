893c861dbdc09dbdba7f1173b2c2517a40db9c16
jiang490094 <jfr08100810@gmail.com>
2020-09-24 20:21:11 +0800

    0924 the end

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use( express.urlencoded({extended: false}) );
+app.use( express.json() );
+
+
 app.get('/', (req, res)=>{
     // res.send('<h2>Hola </h2>');
     res.render('home', {name: 'Shinder'});
@@ -14,9 +18,30 @@ app.get('/json-sales', (req, res)=>{
     // res.json(sales);
     res.render('json-sales', {sales})
 });
+app.get('/json-sales2', (req, res)=>{
+    const sales = require(__dirname + '/../data/sales');
+    // res.json(sales);
+    res.render('abc/def/json-sales2', {sales})
+});
+
+app.get('/try-qs', (req, res)=>{
+    res.json(req.query);
+});
 
 
-app.use(express.static(__dirname + '/../public'));
+app.post('/try-post',(req, res)=>{
+    res.json(req.body);
+});
+
+app.get('/try-post-form',(req, res)=>{
+    res.render('try-post-form');
+});
+
+app.post('/try-post-form',(req, res)=>{
+    res.render("try-post-form", req.body);
+});
+
+app.use( express.static(__dirname + '/../public'));
 
 app.use((req, res )=>{
     res
@@ -26,5 +51,5 @@ app.use((req, res )=>{
 });
 
 app.listen(3000, ()=>{
-    console.log('伺服器已啟動...');
+    console.log('伺服器已啟動...喵');
 })
