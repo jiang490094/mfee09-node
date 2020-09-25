c4077405c070662945da6b4e3e87c424831ed8f6
jiang490094 <jfr08100810@gmail.com>
2020-09-25 16:31:21 +0800

    0925

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const {v4: uuidv4} = require('uuid');
+const axios = require('axios');
+const upload = multer({dest: __dirname + '/../tmp_uploads'});
 
 const app = express();
 
@@ -6,6 +11,10 @@ app.set('view engine', 'ejs');
 
 app.use( express.urlencoded({extended: false}) );
 app.use( express.json() );
+app.use((req, res, next)=>{
+res.locals.title = '小新喵喵店';
+next();
+});
 
 
 app.get('/', (req, res)=>{
@@ -15,6 +24,7 @@ app.get('/', (req, res)=>{
 
 app.get('/json-sales', (req, res)=>{
     const sales = require(__dirname + '/../data/sales');
+    res.locals.title += ' -json';
     // res.json(sales);
     res.render('json-sales', {sales})
 });
@@ -38,7 +48,88 @@ app.get('/try-post-form',(req, res)=>{
 });
 
 app.post('/try-post-form',(req, res)=>{
-    res.render("try-post-form", req.body);
+    res.render('try-post-form', req.body);
+});
+
+app.post('/try-upload', upload.single('avatar'), (req, res)=>{
+    console.log(req.file);
+
+    if(req.file && req.file.originalname){
+        let ext = '';
+
+        switch(req.file.mimetype){
+            case 'image/png':
+            case 'image/jpeg':
+            case 'image/gif':
+
+                fs.rename(
+                    req.file.path,
+                    __dirname + '/../public/img/' + req.file.originalname,
+                    error=>{
+
+                        return res.json({
+                            success: true,
+                            path: '/img/'+ req.file.originalname
+                        });
+                    });
+
+                break;
+            default:
+                fs.unlink(req.file.path, error=>{
+                    return res.json({
+                        success: false,
+                        msg: '不是圖檔'
+                    });
+                });
+
+        }
+    } else {
+        return res.json({
+            success: false,
+            msg: '沒有上傳檔案'
+        });
+    }
+});
+
+app.get('/try-uuid',(req, res)=>{
+    res.json({
+        uuid1: uuidv4(),
+        uuid2: uuidv4(),
+    });
+});
+
+const upload2 = require(__dirname + '/upload-img-module');
+app.post('/try-upload2', upload2.single('avatar'), (req, res)=> {
+    res.json(req.file);
+});
+
+app.get('/my-params1/:action?/:id?', (req, res)=> {
+    res.json(req.params);
+});
+app.get('/my-params2/*?/*?', (req, res)=> {
+    res.json(req.params);
+});
+
+// app.get(/^\/09\d{2}\-?\d{3}\-?\d{3}$/, (req, res)=>{
+//     let u = req.url.slice(1);
+//     u = u.split('?')[0];
+//     u = u.split('-').join('');
+//     res.send(u);
+//     });
+
+
+app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req, res)=> {       let u = req.url.slice(3).split('?')[0];
+        u = u.replace(/-/g, '');
+        res.send(u);
+    });
+
+app.use(require(__dirname + '/routes/admin2'));
+
+app.use('/members', require(__dirname + '/routes/admin3'));
+
+app.get('/yahoo', async (req, res)=>{
+    const response = await axios.get('https://tw.yahoo.com/');
+    res.send(response.data);
 });
 
 app.use( express.static(__dirname + '/../public'));
